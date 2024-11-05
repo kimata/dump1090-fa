fa8ddf9011d212c2654c93805361d68c00426c64
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-11-05 22:56:47 +0900

    fix: ログを抑制

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 mkdir -p /run/dump1090
 
 /usr/sbin/nginx -c /opt/dump1090/nginx.conf 2>/dev/null
-/opt/dump1090/dump1090 --net --write-json /run/dump1090
+/opt/dump1090/dump1090 --quiet --stats-every 60 --net --write-json /run/dump1090
