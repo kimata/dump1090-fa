d3cf24f1f7b09dd78602eb59d6feba5d851016cc
KIMATA Tetsuya <kimata@green-rabbit.net>
2024-11-05 22:49:12 +0900

    fix: ターミナルなしでも動くようにした

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 mkdir -p /run/dump1090
 
 /usr/sbin/nginx -c /opt/dump1090/nginx.conf 2>/dev/null
-/opt/dump1090/dump1090 --net --interactive --write-json /run/dump1090
+/opt/dump1090/dump1090 --net --write-json /run/dump1090
